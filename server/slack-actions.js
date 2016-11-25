98a586ed22d4723237a455803ec32750751e147d
Deepak Prabhakara <deepak@redsift.io>
2016-11-25 20:57:06 +0000

    send back response when actions is received.

--- server/slack-actions.js
+++ server/slack-actions.js
@@ -4,22 +4,26 @@
 
 'use strict';
 
+var slack = require('./slack.js');
+
 // Entry point for DAG node
 module.exports = function (got) {
   /* jshint camelcase: false */
   /* jshint -W069 */
   console.log('slack-actions.js:... got', JSON.stringify(got));
   var inData = got['in'];
-
+  var promises = [];
   for (var d of inData.data) {
     console.log('slackbot.js: data: ', d);
     if (d.value) {
       try {
         var msg = JSON.parse(d.value);
-        console.log('slac-actions.js: msg: ', msg);
+        console.log('slack-actions.js: msg: ', msg);
+        promises.push(slack.postMessage('#'+msg.channel.id, 'Hello from Actions Bot'));
       } catch (err) {
 
       }
     }
   }
+  return promises;
 };
